279d0b8468b75517a11b13a0a984be44cda2ae86
ameer017 <rajiabdullahi907@gmail.com>
2024-10-20 20:40:18 +0100

    header added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div >Header</div>
-  )
-}
+    <header className="w-full border-b-2 p-6 flex justify-between items-center">
+      <div>
+      <Link to="/">
 
-export default Header
+        <img src="/EarthFi.png"/>
+      </Link>
+      </div>
+
+      <nav className="  flex justify-between items-center">
+        <Link to="/about-us" className="px-4 text-[16px] ">About</Link>
+        {/* <Link to="/about" className="px-4 text-[16px] ">EarthFi</Link> */}
+        <Link to="/create-order" className="px-4 text-[16px] ">Create Order</Link>
+        <Link to="/market-place" className="px-4 text-[16px] ">Market Place</Link>
+
+        <button className="bg-[#F29F05] text-[16px] text-[#fff] p-[10px] rounded-lg ">Connect Wallet</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/components/Impact.jsx
+++ frontend/src/components/Impact.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+
+const media = [];
 
 const Impact = () => {
   return (
-    <div>Impact</div>
-  )
-}
+    <div
+      className=" px-4 py-2 flex items-center  gap-[12rem]  "
+      // style={{
+      //   backgroundImage: `url("/union.png")`,
+      //   backgroundRepeat: "no-repeat",
+      //   backgroundPosition: "bottom right",
+      //   backgroundSize: "contain",
+      // }}
+    >
+      <div className="flex flex-col gap-2 p-4 border-2 ">
+        <div className="border-2 border-[#006546] p-6 rounded-lg bg-white w-[390px] h-[209px] flex flex-col  items-center justify-center">
+          <h1 className="text-[28px] ">Environmental Impact</h1>
 
-export default Impact
+          <ul className="text-[16px] mt-3  text-[#7C7C7C] ">
+            <li>Improved Recycling Infrastructure.</li>
+            <li>New recycling points.</li>
+            <li>Greater recovery and recycling capabilities.</li>
+            <li>Cleaner environment.</li>
+          </ul>
+        </div>
+
+        <div className="p-6 rounded-lg bg-[#006546] text-white ml-[18rem] w-[390px] h-[209px] flex flex-col  items-center justify-center -mt-[40px] ">
+          <h1 className="text-[28px] ">Social Impact</h1>
+
+          <ul className="text-[16px] mt-3 text-[#fff] ">
+            <li>Dignified working opportunities.</li>
+            <li>Empowerment of individuals in the workforce.</li>
+            <li>Education and workshops.</li>
+          </ul>
+        </div>
+
+        <div className="border-2 border-[#006546] p-6 rounded-lg bg-white w-[390px] h-[209px] flex flex-col  items-center justify-center -mt-[40px]">
+          <h1 className="text-[28px] ">Community Impact</h1>
+
+          <ul className="text-[16px] mt-3 text-[#7C7C7C] ">
+            <li>Achieve sustainability goals.</li>
+            <li>Easily showcase impact.</li>
+            <li>Attract new customers.</li>
+            <li>New source of revenue.</li>
+          </ul>
+        </div>
+      </div>
+      <div className=" w-[396px]  ">
+        <img src="" />
+      </div>
+    </div>
+  );
+};
+
+export default Impact;
