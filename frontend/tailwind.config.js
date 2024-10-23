c60e59b64a524ce78c3eac6ddcc0dff9792d82fc
Abubakar kehinde <abubakarkehinde88@gmail.com>
2024-10-23 10:14:25 +0100

    Team-page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ['Ubuntu', 'Source Sans Pro'],
+      },
+    },
   },
   plugins: [],
 }
