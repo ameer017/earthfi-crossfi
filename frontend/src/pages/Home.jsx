193d13ef5bad6ba26f68b7107ef265075cfdf604
ameer017 <rajiabdullahi907@gmail.com>
2024-12-14 20:08:06 +0100

    files optimized

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
-import HeroSection from "../components/HeroSection";
-import Impact from "../components/Impact";
-import Partner from "../components/Partner";
-import Ecology from "../components/Ecology";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import {
+  Ecology,
+  Footer,
+  Header,
+  HeroSection,
+  Impact,
+  Partner,
+} from "../index";
 
 const Home = () => {
   return (
@@ -11,12 +13,10 @@ const Home = () => {
       <Header />
       <div className="flex flex-col ">
         <HeroSection />
-
         <div className=" border-t-2 p-4 bg-gradient-to-b from-green-50 to-white ">
           <Ecology />
           <Impact />
         </div>
-
         <Partner />
       </div>
       <Footer />
